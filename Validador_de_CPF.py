164a0ff005901f7abdda14a7aa08b6d226b7284c
nicolasgerbes <nicolasgerbes18@gmail.com>
2025-08-28 12:26:50 -0300

    :tada: Commit Inicial

---
+++ Validador_de_CPF.py
@@ -0,0 +1,40 @@
+import sys
+
+cpf_enviado_usuario = '746.824.890-70' \
+    .replace('.', '') \
+    .replace('-', '') \
+    .replace(' ', '')
+
+cpf_sequencial = cpf_enviado_usuario == cpf_enviado_usuario[0] * len(cpf_enviado_usuario)
+
+if cpf_sequencial:
+    print('Você enviou dados sequenciais.')
+    sys.exit()
+
+nove_digitos = cpf_enviado_usuario[:9]
+contador_regressivo_1 = 10
+
+resultado_1 = 0
+for digito in nove_digitos:
+    resultado_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+digito_1 = (resultado_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_2 = 0
+for digito in dez_digitos:
+    resultado_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é válido!')
+else:
+    print('CPF inválido!')
+
